ffa8da19f7904cebc8ca7cc0d159a6a2c68f38d8
nxcmfg <haydenmccarthy19@gmail.com>
2023-05-26 08:27:42 -0400

    Update license and inheritance

--- __manifest__.py
+++ __manifest__.py
@@ -6,6 +6,7 @@
     'description': 'This custom module was developed for Next Chapter Manufacturing to prompt users with checklists on Quotations and Sales Orders. The addon includes stop-gates to prevent order confirmation prior to checklists completion.',
     'author': 'Hayden McCarthy',
     'website': 'https://www.nxcmfg.com',
+    'license': 'AGPL-3',
     'depends': ['sale'],
     'data': [
         'views/view_nxc_sales_checklist_order_form.xml',
